86803c3ae17eb3ca7e239feb29ddaafe764d1a8e
Darshan Suthar <darshansu156@gmail.com>
2022-10-24 16:13:00 +0530

    Initial cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="bg-black text-red-700">Black</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center text-white items-center">
+      Black
+    </div>
+  );
 };
 
 export default Home;
